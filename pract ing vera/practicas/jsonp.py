817d78a19132954acbbb04ae396c6223297fbef8
Josettz <correoparaestudiosjose@gmail.com>
2026-04-26 01:58:11 -0500

    v6

--- pract ing vera/practicas/jsonp.py
+++ pract ing vera/practicas/jsonp.py
@@ -1,2 +1,14 @@
 import json
-## PENDIENTE
+
+estudiantes = [
+    {"nombre": "Ana", "nota": 9},
+    {"nombre": "Luis", "nota": 4}
+]
+
+# guardar
+with open("estudiantes.json", "w") as f:
+    json.dump(estudiantes, f, indent=4)  # indent=4 lo hace legible
+
+# cargar
+with open("estudiantes.json", "r") as f:
+    estudiantes = json.load(f)
